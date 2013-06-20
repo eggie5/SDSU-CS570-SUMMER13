4f5e69bf2f53bc445ef101f0f6f9ff650ccd8817
Alex Egg <eggie5@gmail.com>
2013-06-19 17:51:25 -0700

    added page table len option

--- A2/A2/ui.c
+++ A2/A2/ui.c
@@ -9,16 +9,19 @@
 #include <stdio.h>
 #include "clock.h"
 #include "utils.h"
-#define PAGE_TABLE_LEN 3
+#define PAGE_T_LEN 3
 
 void ui_loop()
 {
     printf("Assignment 2\n");
+    int page_table_len=PAGE_T_LEN;
+    printf("\nEnter page file length:");
+    scanf("%d",&page_table_len);
     
     unsigned _pages[100];
     int len = read_page_file(&_pages);
-    int page_table_len=PAGE_TABLE_LEN;
-    page_table_len=6;
+
+
     
     printf("pages: ");
     for(int j=0;j<len;j++)
